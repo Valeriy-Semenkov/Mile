e38ddb591352bb19d7958300ccfd80dd72ea2bc0
Valeriy Semenkov <unfairlly@gmail.com>
2021-03-14 06:52:20 +0300

    123

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 24586;
+        int amount = 20;
+
+        int bonus = price / amount;
+        System.out.println(bonus);
+
+
+    }
+}
